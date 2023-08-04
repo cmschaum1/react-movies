56d08d51e334465643059e339de68718e7fc6e97
cmschaum1 <cmschaum@yahoo.com>
2023-08-04 13:37:26 -0400

    Section 15 Complete 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/AddMovie.js 	new file:   src/components/AddMovie.module.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+
 import "./App.css";
 
-const url = "https://swapi.dev/api/";
+//const url = "https://swapi.dev/api/";
+const url = "https://react-movies-b6007-default-rtdb.firebaseio.com/";
 
 const getUrl = (endpoint) => {
   return url + endpoint;
@@ -20,29 +23,82 @@ const transformMovie = (movie) => {
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMovies() {
-    const response = await fetch(getUrl("films"));
-    const data = await response.json();
+    setIsLoading(true);
+    setError(null);
 
-    const transformedMovies = data.results.map((movie) => {
-      return transformMovie(movie);
-    });
-    setMovies(transformedMovies);
+    try {
+      const endpoint = getUrl("movies.json");
+      //console.log(endpoint);
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+
+      const transformedMovies = [];
+      for (const key in data) {
+        transformedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        }) //transformMovie(data[key]))
+      }
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   }
 
-  async function fetchHandler() {
+  const fetchHandler = useCallback(async () => {
     await fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    fetchHandler();
+  }, [fetchHandler]);
+
+  function addMovieHandler(movie) {
+    //const endpoint = 
+    const response = fetch(
+      getUrl("movies.json"), {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: { "Content-Type": "application/json" },
+      })
+    console.log(response)
+  }
+
+  let content = <p>No Movies found.</p>;
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={movies} />
+        <button onClick={fetchHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
